0d3db8a0ea8cb0317268eb0002797e4ed2713833
Westin Lohne <wlohne@andrew.cmu.edu>
2015-04-27 19:58:33 -0400

    current working

--- final.ino
+++ final.ino
@@ -58,6 +58,8 @@ unsigned long switchTime;
 uint32_t userColor = ring.Color(235,156,49);
 int userPixel = 7;
 boolean performedFlurish = false;
+float currTemp;
+float desiredTemp;
 
 void setup(void)
 {
@@ -66,10 +68,12 @@ void setup(void)
   int pentReading = findPetentiometerPercentage();
   // thermometer setup
   sensors.begin();
+  sensors.requestTemperatures();
+  currTemp = sensors.getTempFByIndex(0);
   
   // lightring setup
   ring.begin();
-  ring.setBrightness(24);
+  ring.setBrightness(15);
   ring.show();
 
   pinMode(SPEAKER, OUTPUT);
@@ -80,8 +84,9 @@ void setup(void)
 void loop(void)
 { 
   time = millis();
-   Serial.println((time - switchTime), DEC);
-  if(abs(findPetentiometerPercentage() - pentReading) > 1){
+//   Serial.println((time - switchTime), DEC);
+//  Serial.println(round(desiredTemp/currTemp) * 100);
+  if(abs(findPetentiometerPercentage() - pentReading) > 2){
     pentReading = findPetentiometerPercentage();
     tempSelection();
     switchTime = millis();
@@ -89,16 +94,29 @@ void loop(void)
   }
   else if((time - switchTime) > 1500){
     if(performedFlurish == false){
+      
+      sensors.requestTemperatures();
+      currTemp = sensors.getTempFByIndex(0);
+      
       fillPixelsFrom(userPixel, userColor);
-      setRingToPercentage(ring.Color(30,0,0), 70, userColor, 30);
+      setRingToPercentage(ring.Color(30,0,0), 100 - round((desiredTemp - 100)/(currTemp - 100) * 100), userColor, 30);
       performedFlurish = true;
     }
-  Serial.println("Requesting temperatures...");
-  sensors.requestTemperatures();
-//  Serial.println("DONE");
-//  pulseSection(getLightAmountFor(70), ring.numPixels(), blue_arr);
-  Serial.print("Temperature is: ");
-  Serial.print(sensors.getTempFByIndex(0));
+   if((time % 5000) == 0){
+//      Serial.println("Requesting temperatures...");
+      sensors.requestTemperatures();
+      currTemp = sensors.getTempFByIndex(0);
+    //  Serial.println("DONE");
+    //  pulseSection(getLightAmountFor(70), ring.numPixels(), blue_arr);
+      Serial.print("Temperature is: ");
+//      currTemp = sensors.getTempFByIndex(0);
+      Serial.println(currTemp, DEC);
+      Serial.print("Desired is: ");
+      Serial.println(desiredTemp, DEC);
+      Serial.print("Rounded that makes: ");
+      Serial.println(round((desiredTemp - 100)/(currTemp - 100) * 100));
+      setRingToPercentage(ring.Color(30,0,0), 100 - round((desiredTemp - 100)/(currTemp - 100) * 100), userColor, 30);
+   }
   }
   // temperature requesting
 //  Serial.println("Requesting temperatures...");
@@ -197,6 +215,7 @@ void tempSelection()
     colorAllPixelsBut(1, dull_white);
     userColor = fade14;
     userPixel = 1;
+    desiredTemp = 113;
     
   }
   if(perc > 6 && perc < 12){
@@ -204,84 +223,98 @@ void tempSelection()
     colorAllPixelsBut(2, dull_white);
     userColor = fade13;
     userPixel = 2;
+    desiredTemp = 117;
   }
   if(perc > 12 && perc < 20){
     ring.setPixelColor(3, fade12);
     colorAllPixelsBut(3, dull_white);
     userColor = fade12;
     userPixel = 3;
+    desiredTemp = 121;
   }
   if(perc > 20 && perc < 26){
     ring.setPixelColor(4, fade11);
     colorAllPixelsBut(4, dull_white);
     userColor = fade11;
     userPixel = 4;
+    desiredTemp = 125;
   }
   if(perc > 26 && perc < 32){
     ring.setPixelColor(5, fade10);
     colorAllPixelsBut(5, dull_white);
     userColor = fade10;
     userPixel = 5;
+    desiredTemp = 130;
   }
   if(perc > 32 && perc < 40){
     ring.setPixelColor(6, fade9);
     colorAllPixelsBut(6, dull_white);
     userColor = fade9;
     userPixel =6;
+    desiredTemp = 135;
   }
   if(perc > 40 && perc < 47){
     ring.setPixelColor(7, fade8);
     colorAllPixelsBut(7, dull_white);
     userColor = fade8;
     userPixel = 7;
+    desiredTemp = 140;
   }
   if(perc > 47 && perc < 53){
     ring.setPixelColor(8, fade7);
     colorAllPixelsBut(8, dull_white);
     userColor = fade7;
     userPixel = 8;
+    desiredTemp = 144;
   }
   if(perc > 53 && perc < 60){
     ring.setPixelColor(9, fade6);
     colorAllPixelsBut(9, dull_white);
     userColor = fade6;
     userPixel = 9;
+    desiredTemp = 148;
   }
   if(perc > 60 && perc < 66){
     ring.setPixelColor(10, fade5);
     colorAllPixelsBut(10, dull_white);
     userColor = fade5;
     userPixel = 10;
+    desiredTemp = 155;
   }
   if(perc > 66 && perc < 72){
     ring.setPixelColor(11, fade4);
     colorAllPixelsBut(11, dull_white);
     userColor = fade4;
     userPixel = 11;
+    desiredTemp = 160;
   }
   if(perc > 72 && perc < 78){
     ring.setPixelColor(12, fade3);
     colorAllPixelsBut(12, dull_white);
     userColor = fade3;
     userPixel = 12;
+    desiredTemp = 164;
   }
   if(perc > 78 && perc < 86){
     ring.setPixelColor(13, fade2);
     colorAllPixelsBut(13, dull_white);
     userColor = fade2;
     userPixel = 13;
+    desiredTemp = 168;
   }
   if(perc > 86 && perc < 92){
     ring.setPixelColor(14, fade1);
     colorAllPixelsBut(14, dull_white);
     userColor = fade1;
     userPixel = 14;
+    desiredTemp = 172;
   }
   if(perc > 92){
     ring.setPixelColor(15, fade0);
     colorAllPixelsBut(15, dull_white);
     userColor = fade0;
     userPixel = 15;
+    desiredTemp = 176;
   }
   ring.show();
 }
